b4c36d70a13e3d37f165837ee982b5acd834ea52
Molly Waggett & Summer Brochtrup <student@epicodus.com>
2015-09-14 11:10:13 -0700

    Write functions to remove punctuation and whitespace from string, calculate rectangle width, and rearrange string; specs are met.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-// var functionName = function(params) {
-//
-// };
+var cryptosquare = function(userEntry) {
+
+};
+
+var stripString = function(userEntry) {
+  return userEntry.replace(/[^\w]|_/g, "").toLowerCase();
+
+};
+
+var calculateRectangleWidth = function(number) {
+  return Math.ceil(Math.sqrt(number));
+}
+
+var reconstructString = function(string, number) {
+  var stringArray = string.split("");
+  var newArray = [];
+  for (var count = 0; count < number; count ++) {
+    for (var i = count; i < stringArray.length; i += number) {
+      newArray.push(stringArray[i]);
+    }
+  }
+  return newArray;
+  //return newArray.toString().replace(/,/g, "");
+}
+
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,22 @@
-// describe('functionName', function() {
-//   it("plain English description of spec", function() {
-//     expect(functionName(argument)).to.equal(value);
-//   });
-// });
+describe('stripString', function() {
+  it("puts string to lowercase and removes punctuation and whitespace", function() {
+    expect(stripString("Molly & Summer!")).to.equal("mollysummer");
+  });
+});
+
+describe('calculateRectangleWidth', function() {
+  it("returns rounded up square side length", function() {
+    expect(calculateRectangleWidth(5)).to.equal(3);
+  });
+
+  it("returns side length of perfect square", function() {
+    expect(calculateRectangleWidth(4)).to.equal(2);
+  });
+});
+
+describe('reconstructString', function() {
+  it("returns array of characters in new order", function() {
+    expect(reconstructString("abcdefgh", 3)).to.eql(["a", "d", "g", "b", "e", "h", "c", "f"]);
+    //expect(reconstructString("abcdefgh", 3)).to.equal("adgbehcf");
+  });
+});
